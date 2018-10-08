0230451ac80d1489b139a8f08b62482540e933f4
RJ Beers <r33beers@users.noreply.github.com>
2018-10-08 07:18:29 -0400

    Update all dependencies (#17)
    * Update all dependencies
    
    * Swap out closure compiler for Babel
    * Fix sourcemaps for JS and CSS
    * Remove unused devDependencies
    
    * Update electron to Oct 7 release version

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow } = require('electron');
 
 //Module to set app icon
 const path = require('path');
@@ -11,7 +9,7 @@ let mainWindow;
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 500, height: 352, icon: path.join(__dirname, 'pomodoro-app-icon.png'), resizable: false});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
