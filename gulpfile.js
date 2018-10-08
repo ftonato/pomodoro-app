338de85e6bb0674f81da564f31bf31c0eedf5c8c
Ademílson F. Tonato <ademilsonft@outlook.com>
2018-10-08 13:33:56 -0300

    Update gulp dependency

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,9 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const pump = require('pump');
 
-gulp.task('css', function() {
-  return gulp.src('src/css/main.css')
+gulp.task('css', () => {
+  return gulp
+    .src('src/css/main.css')
     .pipe(sourcemaps.init())
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleanCSS())
@@ -17,27 +18,25 @@ gulp.task('css', function() {
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('js', function(cb) {
-  pump([
-    gulp.src('src/js/*.js', {base: './'}),
-    sourcemaps.init(),
-    babel({
-      presets: ['@babel/env']
-    }),
-    concat('main.min.js'),
-    uglify(),
-    sourcemaps.write('.'),
-    gulp.dest('dist/js')
-  ],
-  cb
+gulp.task('js', () => {
+  pump(
+    [
+      gulp.src('src/js/*.js', { base: './' }),
+      sourcemaps.init(),
+      babel({
+        presets: ['@babel/env']
+      }),
+      concat('main.min.js'),
+      uglify(),
+      sourcemaps.write('.'),
+      gulp.dest('dist/js')
+    ],
+    cb
   );
 });
 
-gulp.task('clean', function() {
-  return gulp.src(['dist/css', 'dist/js'], {read: false})
-    .pipe(clean());
+gulp.task('clean', () => {
+  return gulp.src(['dist/css', 'dist/js'], { read: false, allowEmpty: true }).pipe(clean());
 });
 
-gulp.task('default', ['clean'], function() {
-  gulp.run('css', 'js');
-});
+gulp.task('default', gulp.series(['clean', 'css', 'js']));
