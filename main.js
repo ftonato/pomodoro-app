516f02852bd76ed62b6b655f2c1591e21a8aac8b
Ademílson F. Tonato <ademilsonft@outlook.com>
2018-10-09 14:36:10 -0300

    Update gitignore, gulpfile, remove dist directory, update timer script and general refactorings

--- main.js
+++ main.js
@@ -2,19 +2,20 @@
 
 const { app, BrowserWindow } = require('electron');
 
-//Module to set app icon
+// Module to set app icon
 const path = require('path');
+const _ICON = 'pomodoro-app-icon.png';
 
 let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 500, height: 352, icon: path.join(__dirname, 'pomodoro-app-icon.png'), resizable: false});
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 500, height: 352, icon: path.join(__dirname, _ICON), resizable: false });
   mainWindow.loadFile('index.html');
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
 
@@ -23,13 +24,13 @@ function createWindow () {
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
--- src/js/timer.js
+++ src/js/timer.js
@@ -4,6 +4,9 @@
 
 const notifier = require('node-notifier');
 const path = require('path');
+const _NAME = 'Pomodoro App';
+const _ICON = 'pomodoro-app-icon.png';
+const _BEEP = new Audio('endbeep.wav');
 
 new Vue({
   el: '#app',
@@ -15,60 +18,23 @@ new Vue({
     message: ['Pomodoro Complete', 'Breaktime Complete']
   },
   ready() {
-    let self = this;
-    window.addEventListener('keyup', function(evt) {
-      evt = evt || window.event;
-      if (evt.ctrlKey && evt.keyCode == 82) {
-        self.pause();
-      }
-      if (evt.ctrlKey && evt.keyCode == 83) {
-        self.start();
-      }
-      if (evt.ctrlKey && evt.keyCode == 49) {
-        if (self.enableTimer()) {
-          self.time = 300;
-          self.initial = 300;
-        }
-      }
-      if (evt.ctrlKey && evt.keyCode == 50) {
-        if (self.enableTimer()) {
-          self.time = 600;
-          self.initial = 600;
-        }
-      }
-      if (evt.ctrlKey && evt.keyCode == 51) {
-        if (self.enableTimer()) {
-          self.time = 1500;
-          self.initial = 1500;
-        }
-      }
+    window.addEventListener('keyup', event => {
+      event = event || window.event;
+      this.mapButtonEvents(event);
     });
-
   },
   filters: {
     minutesAndSeconds() {
       let minutes = Math.floor(this.time / 60);
       let seconds = this.time - 60 * minutes;
 
-      if (minutes < 10) {
-        minutes = '0' + Math.floor(this.time / 60);
-      } else if (minutes === 0) {
-        minutes = '00';
-      }
+      ({ minutes, seconds } = this.addZerosLeft(this.time, minutes, seconds));
 
-      if (seconds < 10) {
-        seconds = '0' + (this.time - 60 * minutes);
-      } else if (seconds === 0) {
-        seconds = '00';
-      }
-
-      return minutes + ':' + seconds;
+      return `${minutes}:${seconds}`;
     }
   },
   methods: {
     start() {
-      let beeps = new Audio('endbeep.wav');
-
       this.interval = setInterval(() => {
         this.time -= 1;
         if (this.time === 0 && this.breaktime === false) {
@@ -77,7 +43,7 @@ new Vue({
           this.time = 300;
           this.initial = 300;
           this.showNotification(true);
-          beeps.play();
+          _BEEP.play();
           clearInterval(this.interval);
         } else if (this.time === 0 && this.breaktime === true) {
           this.started = false;
@@ -85,7 +51,7 @@ new Vue({
           this.time = 1500;
           this.initial = 1500;
           this.showNotification(false);
-          beeps.play();
+          _BEEP.play();
           clearInterval(this.interval);
         }
       }, 1000);
@@ -98,7 +64,7 @@ new Vue({
 
     enableTimer() {
       let self = this;
-      let enableToChangeTimer = (self.started === false && self.breaktime === false);
+      let enableToChangeTimer = self.started === false && self.breaktime === false;
 
       if (!enableToChangeTimer) {
         self.message[0] = 'Impossible to change the "timer" while the Pomodoro or Breaktime happens!';
@@ -112,12 +78,47 @@ new Vue({
       let message = complete ? this.message[0] : this.message[1];
 
       notifier.notify({
-        title: 'Pomodoro App',
-        message: '\n'+message,
-        icon: path.join(__dirname, 'pomodoro-app-icon.png'),
-        time: 3000,
+        title: _NAME,
+        message,
+        icon: path.join(__dirname, _ICON),
+        time: 3000
       });
+    },
+    mapButtonEvents(evt) {
+      if (evt.ctrlKey && evt.keyCode == 82) {
+        this.pause();
+      }
+      if (evt.ctrlKey && evt.keyCode == 83) {
+        this.start();
+      }
+      if (evt.ctrlKey && evt.keyCode == 49) {
+        this.setTimeAndInitial(300);
+      }
+      if (evt.ctrlKey && evt.keyCode == 50) {
+        this.setTimeAndInitial(600);
+      }
+      if (evt.ctrlKey && evt.keyCode == 51) {
+        this.setTimeAndInitial();
+      }
+    },
+    setTimeAndInitial(time = 1500) {
+      if (this.enableTimer()) {
+        this.time = time;
+        this.initial = time;
+      }
+    },
+    addZerosLeft(time, minutes, seconds) {
+      if (minutes < 10) {
+        minutes = '0' + Math.floor(time / 60);
+      } else if (minutes === 0) {
+        minutes = '00';
+      }
+      if (seconds < 10) {
+        seconds = '0' + (time - 60 * minutes);
+      } else if (seconds === 0) {
+        seconds = '00';
+      }
+      return { minutes, seconds };
     }
   }
-
 });
